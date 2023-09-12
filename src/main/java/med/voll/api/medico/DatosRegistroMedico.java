a7a49ae0c5d1132b084823428b675321f8867844
AngelMasterr <angelduarteprog@gmail.com>
2023-09-11 23:47:57 -0500

    Configurando la conexion a la BD mysql con spring boot

--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -3,6 +3,5 @@ package med.voll.api.medico;
 import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad,
-		DatosDireccion direccion) {
-	
+		DatosDireccion direccion) {	
 }
