7c4fb6eef073100a65eaee5a3c2d0dae9b661e7f
AngelMasterr <angelduarteprog@gmail.com>
2023-09-13 23:33:43 -0500

    spring data, usando la dependencia validation

--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,7 +1,29 @@
 package med.voll.api.medico;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.direccion.DatosDireccion;
 
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad,
+@SuppressWarnings("deprecation")
+public record DatosRegistroMedico(
+		// dependency validation: 
+		// @NOtBlank: comprueba que el valor no llegue nulo ni esta vacion
+		// @NotNull: comprueba que el valor no llegue nulo
+		// @email: comprueba que sea un email
+		// @Pattern(regexp = "//d{4,6}"): permite solo numeros, de 4 a 6 digitos
+		@NotBlank
+		String nombre,
+		@NotBlank
+		@Email 
+		String email, 
+		@NotBlank
+		@Pattern(regexp = "//d{4,6}")
+		String documento,
+		@NotBlank
+		Especialidad especialidad,
+		@NotNull
 		DatosDireccion direccion) {	
 }
---
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `vollmed_api`.`medicos` (
+
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `documento` VARCHAR(100) NOT NULL,
+  `especialidad` VARCHAR(100) NOT NULL,
+  `calle` VARCHAR(100) NOT NULL,
+  `distrito` VARCHAR(100) NOT NULL,
+  `complemento` VARCHAR(100) NOT NULL,
+  `numero` VARCHAR(45) NOT NULL,
+  `ciudad` VARCHAR(100) NOT NULL,
+  
+  PRIMARY KEY (`id`)
+);
