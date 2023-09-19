c096f17042add1e4c17e77fe1dc4a56dd23b12ef
AngelMasterr <angelduarteprog@gmail.com>
2023-09-19 00:10:42 -0500

    creando metodos para controlar y visualizar mejor las exceptions

---
+++ src/main/java/med/voll/api/infra/TratadorDeErrores.java
@@ -0,0 +1,32 @@
+package med.voll.api.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+//	cuando se introduce una url que no existe
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity tratarError404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+//	cuando se agrega un registro con datos principales faltantes (nombre, email, documento)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
+		var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+		return ResponseEntity.badRequest().body(errores);
+	}
+	
+	private record DatosErrorValidacion(String campo, String error) {
+		public DatosErrorValidacion(FieldError error) {
+			this(error.getField(), error.getDefaultMessage());
+		}
+	}
+}
