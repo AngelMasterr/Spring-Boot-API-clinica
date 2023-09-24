b8bb7fa8977707be68f9d3a5998700d2cc567a7e
AngelMasterr <angelduarteprog@gmail.com>
2023-09-23 23:24:11 -0500

    Terminando de configurar los accesos usando el token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -3,6 +3,8 @@ package med.voll.api.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,28 +12,35 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.usuarios.UsuarioRepository;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter{
-	
+public class SecurityFilter extends OncePerRequestFilter {
+
 	@Autowired
 	private TokenService tokenService;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
+
 //		obtener el token del header
-		var token = request.getHeader("Authorization");
-		if (token == null || token == "") {
-			throw new RuntimeException("El token enviado esta vacio o es nulo");
+		var authHeader = request.getHeader("Authorization");
+		if (authHeader != null) {
+			var token = authHeader.replace("Bearer ", "");
+			var subject = tokenService.getSubject(token);
+			if (subject != null) {
+				// token valido
+				var usuario = usuarioRepository.findByLogin(subject);
+				var authentication = new UsernamePasswordAuthenticationToken(usuario, null, 
+						usuario.getAuthorities()); // forzamos un inicio de sesion
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+
 		}
-		token = token.replace("Bearer ", "");
-		System.out.println(token);
-		System.out.println(tokenService.getSubject(token)); 		
 		filterChain.doFilter(request, response);
 	}
-	
-	
 
 }
